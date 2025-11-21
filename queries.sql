1e7fb5c0279924512cfe9603435d261d9cf6511d
/ <candice.chevret@gmail.com>
2025-11-21 15:32:33 +0100

    bonus

--- queries.sql
+++ queries.sql
@@ -4,5 +4,52 @@ SELECT * FROM skills;
 SELECT * FROM resources_skills; */
 
 /* Récupérer la liste des thèmes */
-SELECT * FROM themes;
+/* SELECT * FROM themes; */
+
+/* Récupérer toutes les ressources triées par date de mise à jour */
+/* SELECT *
+FROM resources
+ORDER BY update_at;  */
+
+/* Récupérer uniquement le nom et l'url des ressources de type exercice */
+/* SELECT title, url
+FROM resources
+WHERE type='exercice'; */
+
+/* Récupérer le titre et la description des ressources d'Ada uniquement */
+/* SELECT title, description
+FROM resources
+WHERE is_ada=TRUE */
+
+/* Récupérer les ressources qui ont la compétence JavaScript associée */
+/* SELECT *
+FROM resources
+JOIN resources_skills ON resources.id = resources_skills.resources_id
+JOIN skills ON resources_skills.skill_id = skills.id
+WHERE skills.name='JavaScript'; */
+
+/* Récupérer les ressources dont le titre contient le mot "react" (avec ou sans majuscules) */
+/* SELECT *
+FROM resources
+WHERE title ILIKE '%react%'; */
+
+/* ----------BONUUUUS--------------- */
+
+/*  Récupérer la liste des thèmes avec le nombre de ressources par thème */
+
+/* SELECT themes.name, COUNT(resources.id) AS resource_count
+FROM themes
+LEFT JOIN resources
+    ON resources.theme_id = themes.id
+GROUP BY themes.name; */
+
+/* Récupérer le nom et l'url de toutes les ressources avec un tableau/liste contenant l'ensemble de leurs skills associés */
+
+/* SELECT name, url
+FROM resources
+JOIN resources_skills ON resources.id = resources_skills.resources_id
+JOIN skills ON resources_skills.skill_id = skills.id; */
+
+/* Récupérer les 5 ressources les plus récentes avec leur thème */
+
 
--- seed.sql
+++ seed.sql
@@ -1,27 +1,27 @@
 /* Insert thème */
-/* INSERT INTO themes (id, name, description, created_at, update_at)
+INSERT INTO themes (id, name, description, created_at, update_at)
 VALUES
 (1, 'Frontend', 'La salle du restaurant', '2025-06-13 12:05','2025-06-13 13:10'),
 (2, 'Backend', 'La cuisine', '2025-06-13 12:10','2025-06-13 12:45'),
 (3, 'Base de données', 'Le garde-manger / frigo du restaurant','2025-06-13 12:05','2025-06-13 13:10'),
-(4, 'DevOps', 'Le gestionnaire du restaurant et son organisation', '2025-06-13 08:30','2025-06-13 23:30'); */
+(4, 'DevOps', 'Le gestionnaire du restaurant et son organisation', '2025-06-13 08:30','2025-06-13 23:30');
 
-/* INSERT INTO resources (id, type, title, description, url, is_ada, theme_id, created_at, update_at)
+INSERT INTO resources (id, type, title, description, url, is_ada, theme_id, created_at, update_at)
 VALUES
-(1, 'introduction à React', 'https://react.dev', 'Guide officiel React', 'guide', false, 1, '2025-06-13 12:05','2025-06-13 13:10'),
-(2, 'SQL pour débutant', 'https://example.com/sql', 'Cours SQL complet', 'video', true, 3, '2025-06-13 12:05','2025-06-13 13:10'),
-(3, 'Exercices JavaScript', 'https://example.com/js', 'Pratique JS', 'exercice', true, 1,'2025-06-13 12:05','2025-06-13 13:10'),
-(4, 'Créer une API en node', 'https://example.com/node', 'Projet node', 'projet', false, 2,'2025-06-13 12:05','2025-06-13 13:10'); */
+(1, 'guide', 'introduction à React', 'Guide officiel React', 'https://react.dev',  false, 1, '2025-06-13 12:05','2025-06-13 12:05'),
+(2,  'video', 'SQL pour débutant', 'Cours SQL complet','https://example.com/sql', true, 3, '2025-08-18 12:05','2025-08-18 12:05'),
+(3, 'exercice', 'Exercices JavaScript', 'Pratique JS', 'https://example.com/js',  true, 1,'2025-12-01 12:05','2025-12-01 12:05'),
+(4, 'projet', 'Créer une API en node', 'Projet node', 'https://example.com/node',  false, 2,'2025-02-28 12:05','2025-02-28 12:05');
 
-/* INSERT INTO skills (id, name)
+INSERT INTO skills (id, name)
 VALUES
 (1, 'JavaScript'),
 (2, 'React'),
 (3, 'SQL'),
 (4, 'PostgreSQL'),
-(5, 'Node.js'); */
+(5, 'Node.js');
 
-/* INSERT INTO resources_skills (resources_id, skill_id)
+INSERT INTO resources_skills (resources_id, skill_id)
 VALUES
 (1,1),
 (1,2),
@@ -29,4 +29,4 @@ VALUES
 (2,4),
 (3,1),
 (4,1),
-(4,5); */
+(4,5);
